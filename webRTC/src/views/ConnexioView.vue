9d4f24ccacb6a8dffddb1b6c4b95cd97a7c85520
Alvaro Kumenius <akumenius@gmail.com>
2024-01-22 11:07:37 +0100

    Acabat

--- webRTC/src/views/ConnexioView.vue
+++ webRTC/src/views/ConnexioView.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <div class="container">
+    <div class="container">
+        <div class="connection_container">
             <div>
                 <label>El teu ID</label>
                 <input class="text" v-model="outgoing" type="text">
@@ -14,9 +14,22 @@
                 <button v-if="!isInitiator" @click="senyalarElMeuPeer_2()">2 - SENYALAR EL MEU PEER</button>
                 <button v-if="isInitiator" @click="senyalarElMeuPeer_3()">3 - SENYALAR EL MEU PEER</button>
                 <div>
-                <input type="checkbox" id="checkbox" v-model="isInitiator">
-                <label for="checkbox">Iniciador</label>
+                    <input type="checkbox" id="checkbox" v-model="isInitiator">
+                    <label for="checkbox">Iniciador</label>
+                </div>
             </div>
+        </div>
+        <div class="communication_container">
+            <div>
+                <label>Envia missatge</label>
+                <input class="text" v-model="message" type="text">
+                <button @click="enviarMissatge_4()">4 - ENVIAR MISSATGE</button>
+            </div>
+            <div>
+                <h2>CHAT</h2>
+                <div v-for="actual in array_messages">
+                    {{ actual }}
+                </div>
             </div>
         </div>
     </div>
@@ -35,8 +48,8 @@ export default {
             outgoing: "",
             ingoing: "",
             isInitiator: true,
-            peer_initiator: "",
-            peer_not_initiator: ""
+            my_peer: "",
+            array_messages: []
         }
     },
     methods: {
@@ -58,7 +71,13 @@ export default {
                 this.outgoing = JSON.stringify(data) //le pongo el contenido dentro de una caja de texto*/
             })
             console.log(this.outgoing);
-            this.peer_initiator = peer;
+
+            peer.on("data", (data) => {
+		        console.log("" + data);
+                this.array_messages.push("" + data);
+	        });
+
+            this.my_peer = peer;
         },
         senyalarElMeuPeer_2() {
 
@@ -70,7 +89,7 @@ export default {
             });
 
             console.log(peer);
-
+            peer.signal(this.ingoing);
             peer.on("signal", (data) => {
                 //es crida cada cop que es crea un nou objecte Peer
                 console.log("MY ID TO ACCEPT CALL:");
@@ -82,11 +101,23 @@ export default {
                 }
             });
 
-            this.peer_not_initiator = peer;
+            peer.on("data", (data) => {
+		        console.log("" + data);
+                this.array_messages.push("" + data);
+	        });
+
+            this.my_peer = peer;
         },
         senyalarElMeuPeer_3() {
             console.log("Initiator signaling my peer");
-            this.peer_initiator.signal(this.ingoing);
+            this.my_peer.signal(this.ingoing);
+        },
+        enviarMissatge_4() {
+            var message = this.nom + ": " + this.message;
+            console.log(message);
+            this.my_peer.send(message);
+            this.array_messages.push(message);
+            this.message = "";
         }
     },
     computed: {
@@ -105,6 +136,12 @@ export default {
   
 <style>
 .container {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+
+.connection_container {
     display: flex;
     flex-direction: row;
     gap: 20px;
